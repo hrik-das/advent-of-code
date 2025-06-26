985242c4aba45b66729906d11ea8ca8f72a1202e
hrik-das <emptynull01@gmail.com>
2025-06-26 19:04:42 +0530

    first day complete

--- 2015/Day 1/index.js
+++ 2015/Day 1/index.js
@@ -1,23 +1,42 @@
-const fs = require("fs");
+const fs = require("node:fs");
 
-fs.readFile("input.txt", "utf8", (error, data) => {
-    if (error) {
-        console.error("Error Reading File: ", error);
-        return;
+const readFile = function (file, encode) {
+  let data = fs.readFileSync(file, encode);
+  return data;
+};
+
+const findingFloor = function (instructions) {
+  let count = 0;
+
+  for (let char of instructions) {
+    if (char === "(") {
+      count = count + 1;
+    } else {
+      count = count - 1;
+    }
+  }
+
+  return count;
+};
+
+const findBasementPosition = function (instructions) {
+  let floor = 0;
+
+  for (let i = 0; i < instructions.length; i++) {
+    if (instructions[i] === "(") {
+      floor = floor + 1;
+    } else {
+      floor = floor - 1;
     }
 
-    const ary = data.split("");
-    let floor = 0;
-    let shown = false;
+    if (floor === -1) {
+      return i + 1;
+    }
+  }
+};
 
-    ary.forEach((element, index) => {
-        (element == "(") ? floor++ : floor--;
-        
-        if (floor === -1 && !shown) {
-            console.log(`The First "Floor == -1" Position is: ${index + 1}`);
-            shown = true;
-        }
-    });
+let buffer = readFile("input.txt", "utf-8");
+let data = buffer.trim().split("");
 
-    console.log("The Total Number of Floors: ",floor);
-});
+console.log(`The instructions took santa to the ${findingFloor(data)}th floor`);
+console.log(`The position of the character is ${findBasementPosition(data)}`);
